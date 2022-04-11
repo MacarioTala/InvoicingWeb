cc8e44fb830f3bd6ef4a8e6d59960f30cf281ab0
Macario <Jose@Marici.io>
2022-04-11 14:04:54 -0700

    formatted CustomerInvoice.html to make it easier to read fixed tags in SideBySideSummary.html fixed generator expressions in side_by_side_summary added collapsible to SideBySideSummary.html

--- InvoicingProject/InvoicingWeb/side_by_side_summary.py
+++ InvoicingProject/InvoicingWeb/side_by_side_summary.py
@@ -13,11 +13,14 @@ def customer_invoices_side_by_side(request,customer_name):
 	
 def get_customer_side_by_side_data(customer_name):
 	class side_by_side_row:
-		def __init__(self, CustomerInvoice, PartnerInvoice, PartnerComputedAmount, Margin):
+		def __init__(self,EffectiveYear,EffectiveMonth, CustomerInvoice, PartnerInvoice, PartnerComputedAmount,PartnerStatedAmount, Margin):
 			self.CustomerInvoice=CustomerInvoice
 			self.PartnerInvoice=PartnerInvoice
 			self.PartnerComputedAmount=PartnerComputedAmount
+			self.PartnerStatedAmount=PartnerStatedAmount
 			self.Margin=Margin
+			self.EffectiveMonth=EffectiveMonth
+			self.EffectiveYear=EffectiveYear
 			
 	customer_invoices=get_customer_invoice_data(customer_name)["invoice_list"]
 	
@@ -29,10 +32,34 @@ def get_customer_side_by_side_data(customer_name):
 		partner_stated_amount=customer_invoice.PartnerInvoice.AmountOnInvoice
 		margin=round(((customer_amount.amount-partner_stated_amount.amount)/customer_amount.amount*100),2)
 		
-		side_by_side_item=side_by_side_row(CustomerInvoice=customer_invoice,PartnerInvoice=customer_invoice.PartnerInvoice, PartnerComputedAmount=partner_computed_amount,Margin=margin)
+		side_by_side_item=side_by_side_row(EffectiveYear=customer_invoice.EffectiveYear,
+		EffectiveMonth=customer_invoice.EffectiveMonth,
+		CustomerInvoice=customer_invoice,
+		PartnerInvoice=customer_invoice.PartnerInvoice, 
+		PartnerStatedAmount=partner_stated_amount,
+		PartnerComputedAmount=partner_computed_amount,
+		Margin=margin)
 
 		side_by_side_list.append(side_by_side_item)
-	
-	context= {"side_by_side_list" : side_by_side_list}
+			
+	#Annual Totals
+	class annual_total:
+		def __init__(self,Year,RevenueTotal,StatedPartnerTotal,ComputedPartnerTotal):
+			self.Year=Year
+			self.RevenueTotal=RevenueTotal
+			self.StatedPartnerTotal=StatedPartnerTotal
+			self.ComputedPartnerTotal=ComputedPartnerTotal
+			
+	annual_totals=[]
+	for item in side_by_side_list:
+		if item.EffectiveYear not in (temp.Year for temp in annual_totals):
+			revenue_total = sum((item.CustomerInvoice.InvoiceTotal for item in [x for x in side_by_side_list if x.EffectiveYear==item.CustomerInvoice.EffectiveYear]))
+			stated_partner_total=sum((item.PartnerStatedAmount for item in [x for x in side_by_side_list if x.EffectiveYear==item.CustomerInvoice.EffectiveYear])) 
+			computed_partner_total=sum((item.PartnerComputedAmount for item in [x for x in side_by_side_list if x.EffectiveYear==item.CustomerInvoice.EffectiveYear])) 
+			current_total=annual_total(Year=customer_invoice.EffectiveYear,RevenueTotal=revenue_total,StatedPartnerTotal=stated_partner_total,ComputedPartnerTotal=computed_partner_total)
+			annual_totals.append(current_total)
+			
+	context= {"side_by_side_list" : side_by_side_list,
+	"annual_totals" : annual_totals }
 	
 	return context
